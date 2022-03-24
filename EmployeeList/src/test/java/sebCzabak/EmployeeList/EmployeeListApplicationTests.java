b87fa151506f84649e0b3a1893999e366d11700e
Sebastian <sczabak@gmail.com>
2022-03-24 12:01:20 +0100

    Initial commit

---
+++ EmployeeList/src/test/java/sebCzabak/EmployeeList/EmployeeListApplicationTests.java
@@ -0,0 +1,13 @@
+package sebCzabak.EmployeeList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
